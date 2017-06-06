8e7af8d447ebff837cfe9191c1957c82116aebd0
dbudhwani <Pa55word>
2017-06-06 18:23:29 +0530

    Added travis file to build solution

--- GoBDD/services/github_test.go
+++ GoBDD/services/github_test.go
@@ -9,35 +9,32 @@ import (
 	"errors"
 )
 
-func TestGithub(t *testing.T) {
-	Convey("Github API", t, func() {
-		Convey("Should return repository information", func() {
-			backgroundContext := context.Background()
-			repositoryServices := new(mocks.IRepositoryServices)
-			github := NewGithub(repositoryServices, backgroundContext)
+func TestGithubAPI(t *testing.T) {
+	Convey("Should return repository information", t, func() {
+		backgroundContext := context.Background()
+		repositoryServices := new(mocks.IRepositoryServices)
+		github := NewGithub(repositoryServices, backgroundContext)
 
-			fullName := "ABC"
-			starCount := 10
-			repo := &Repository{
-				FullName:        &fullName,
-				Description:     &fullName,
-				ForksCount:      &starCount,
-				StargazersCount: &starCount,
+		fullName := "ABC"
+		starCount := 10
+		repo := &Repository{
+			FullName:        &fullName,
+			Description:     &fullName,
+			ForksCount:      &starCount,
+			StargazersCount: &starCount,
 
-			}
-			repositoryServices.On("Get", backgroundContext, "golang-coach", "Lessons").Return(repo, nil, nil)
-			pack, _ := github.GetPackageRepoInfo("golang-coach", "Lessons")
-			So(pack.ForksCount, ShouldEqual, starCount)
-		})
+		}
+		repositoryServices.On("Get", backgroundContext, "golang-coach", "Lessons").Return(repo, nil, nil)
+		pack, _ := github.GetPackageRepoInfo("golang-coach", "Lessons")
+		So(pack.ForksCount, ShouldEqual, starCount)
+	})
 
-		Convey("Should return error when failed to retrieve  repository information", func() {
-			backgroundContext := context.Background()
-			repositoryServices := new(mocks.IRepositoryServices)
-			github := NewGithub(repositoryServices, backgroundContext)
-			repositoryServices.On("Get", backgroundContext, "golang-coach", "Lessons").Return(nil, nil, errors.New("Error has been occurred"))
-			_, err := github.GetPackageRepoInfo("golang-coach", "Lessons")
-			So(err, ShouldNotBeEmpty)
-		})
-
+	Convey("Should return error when failed to retrieve  repository information", t, func() {
+		backgroundContext := context.Background()
+		repositoryServices := new(mocks.IRepositoryServices)
+		github := NewGithub(repositoryServices, backgroundContext)
+		repositoryServices.On("Get", backgroundContext, "golang-coach", "Lessons").Return(nil, nil, errors.New("Error has been occurred"))
+		_, err := github.GetPackageRepoInfo("golang-coach", "Lessons")
+		So(err, ShouldNotBeEmpty)
 	})
 }
